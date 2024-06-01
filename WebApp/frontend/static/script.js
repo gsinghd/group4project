0b744ec15cb248255ab9eb79ca43d000edc9f8ab
nnatividad <k4lani@gmail.com>
2024-06-01 01:08:29 -0700

    trying to get map to work

--- WebApp/frontend/static/script.js
+++ WebApp/frontend/static/script.js
@@ -1,1 +1,10 @@
 // script.js
+function initMap() {
+    // Initialize the map
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    // Add the OpenStreetMap tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
